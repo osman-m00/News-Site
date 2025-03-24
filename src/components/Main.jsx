abfb88855b59dec64d1752d539d7c6bf453cc774
unknown <mohamedsr92@gmail.com>
2025-03-24 02:46:18 +0200

    main news section add

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,24 @@ import React from 'react'
 
 const Main = () => {
   return (
-    <div className='border border-black h-100 w-200 mt-20 absolute left-5 rounded-2xl'>
+    <div className="border border-black w-[900px] rounded-2xl overflow-hidden bg-white mt-30 ml-10">
+      
+      {/* Image Section */}
+      <div className="w-full h-[450px]">
+        <img className="w-full h-full object-cover" src="/news.jpg" alt="News" />
+      </div>
+
+      {/* Text Section */}
+      <div className="p-8">
+        <h1 className="text-3xl font-bold">Trump looms over Canada’s election as campaign begins</h1>
+        <p className="text-xl mt-3">
+          The country will vote on 28 April as President Trump wages a trade war and says he wants to make Canada the 51st US state.
+        </p>
+        <div className="text-gray-500 text-lg mt-3">
+          <span>2 hrs ago</span> | <span>US & Canada</span>
+        </div>
+      </div>
+
     </div>
   )
 }
